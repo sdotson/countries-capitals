99352b00095b9287b41b98dc61ed97d000399247
Stu <stuart@blueprintinteractive.com>
2015-07-22 17:58:39 -0400

    Update 2015-07-22T21:58:38.922Z

--- app/app.js
+++
@@ -1,9 +0,0 @@
-angular.module('countriesApp',['countriesList','countryDetail','home','countries','ngAnimate'])
-	.run(function($rootScope, countriesService) {
-		$rootScope.$on('$routeChangeStart', function() {
-      $rootScope.isLoading = true;
-    });
-    $rootScope.$on('$routeChangeSuccess', function() {
-      $rootScope.isLoading = false;
-    });
-	});
