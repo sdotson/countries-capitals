74265e170ce89c4a381666a3e14ec6edbeb6686b
Stu <stuart@blueprintinteractive.com>
2015-07-24 09:42:00 -0400

    more experiments with countrieslist ctrl

--- app/countries-list/countries-listSpec.js
+++ app/countries-list/countries-listSpec.js
@@ -17,4 +17,33 @@ describe("countriesList", function() {
       $httpBackend.verifyNoOutstandingExpectation();
     }));
   });
+
+  describe('CountriesCtrl', function() {
+      var ctrl, scope;
+      beforeEach(inject(function($controller, $rootScope, countriesService) {
+          scope = $rootScope.$new();
+          ctrl = $controller('CountriesCtrl', {
+              $scope : scope
+          });
+      }));
+
+      /*it('should redirect to country detail page', function() {
+        inject(function($location, $rootScope, $httpBackend, $route, countriesService) {
+          $httpBackend.whenGET('country-detail/country-detail.html').respond('...');
+          $httpBackend.expectGET('http://api.geonames.org/countryInfo?type=JSON&username=sdotson2015').respond({});
+
+          $rootScope.$apply(function($location) {
+            scope.redirect({
+              countryName : "France",
+              capital : "Paris"
+            });
+          });
+
+          expect($route.current.loadedTemplateUrl).toBe("countries-list/countries-list.html");
+
+          $httpBackend.verifyNoOutstandingExpectation();
+        });
+      });*/
+  });
+
 });
