5a708bda8e3a7c203a6a6a40aa37e1fe70a0e28b
Stu <stuart@blueprintinteractive.com>
2015-07-26 10:02:22 -0400

    add more tests

--- app/countriesSpec.js
+++ app/countriesSpec.js
@@ -1,4 +1,45 @@
-describe('countriesService', inject(function (countriesService) {
+describe('countriesService', function(countries) {
 	beforeEach(module('countries'));
-  
-}));
+
+  it('getCountries returns object', inject(function (countriesService, $q, $http) {
+    countriesService.getCountries().then(function(data) {
+      console.log(data);
+      assert.isObject(data);
+    });
+  }));
+
+  /*it('getCountry returns object', inject(function (countriesService, $q, $http) {
+   countriesService.countries = [{
+      countryName: 'France',
+      capital: 'Paris'
+    }];
+
+    console.log(countriesService.getCountry);
+
+    countriesService.getCountry('France').then(function(data) {
+      assert.isObject(data);
+    });
+  }));*/
+
+  /*it('getCapital returns object', inject(function (countriesService, $q, $http) {
+    countriesService.getCapital('Paris').then(function(data) {
+      console.log(data);
+      assert.isObject(data);
+    });
+  }));
+
+  it('getNeighbors returns object', inject(function (countriesService, $q, $http) {
+    countriesService.getNeighbors().then(function(data) {
+      console.log(data);
+      assert.isObject(data);
+    });
+  }));
+
+  it('getCountryDetails returns object', inject(function (countriesService, $q, $http) {
+    countriesService.getCountryDetails().then(function(data) {
+      console.log(data);
+      assert.isObject(data);
+    });
+  }));*/
+
+});
